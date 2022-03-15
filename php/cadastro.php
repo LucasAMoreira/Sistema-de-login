1d7821060ec46147f3df67c72607022351993acf
LucasAMoreira <39498462+LucasAMoreira@users.noreply.github.com>
2022-03-15 15:47:14 -0300

    Update comments

--- php/cadastro.php
+++ php/cadastro.php
@@ -1,24 +1,31 @@
 <?php
 
+	/*
+		cadastro.php
+		Recebe as informações do usuário via POST e as cadastra em um banco de dados
+	*/
+
+
 	include "conecta.php";
 
+	// Recebe nome, email e senha de 'cadastro.html'
 	$nome  = $_POST['nome'];
 	$email = $_POST['email'];
 	$senha = $_POST['senha'];
 
+	// Encripta senha do usuário
 	$senhaMD5=md5($senha);
 
+	// Cadastra nome, email e senha encriptada do usuário na tabela 'usuarios'
 	$tabela = "usuarios";
 	$comandoInsere = "INSERT INTO $tabela VALUES('$nome','$senhaMD5','$email')";
 
+	// Se não faltar nenhuma informação vai para resultadoCadastro.html
 	if(!$nome || !$senha || !$email){
-		echo "Erro!";			
+		echo "Erro!";
 	}else{
 		$insere=pg_query($psql,$comandoInsere);
 		header("location:resultadoCadastro.html");
 	}
-
-
-
 
 ?>
--- php/conecta.php
+++ php/conecta.php
@@ -1,13 +1,15 @@
 <?php
-	
+
 	/*
-		Cria objeto do tipo mysqli para conectar ao banco de dados
+		Cria objeto do tipo pg_connect para conectar ao banco de dados
 	*/
-	
+
+	// O valor dessas variáveis pode mudar de acordo com a forma que o banco de dados foi implementado.
+	// Nesse caso o BD está em uma máquina local, chama-se 'voyage' e não tem senha.
 	$servidor="localhost";
 	$usuario="postgres";
 	$banco="voyage";
 	$senha="";
-	
+
 	$psql = pg_connect("host=localhost port=5432 dbname=voyage user=postgres password=postgres");
 ?>
